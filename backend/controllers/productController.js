93381277bdff64c063959f8479bfdfc85e9ff2b6
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-13 16:51:22 -0300

    creat controller products

---
+++ backend/controllers/productController.js
@@ -0,0 +1,50 @@
+
+const Product = require('../models/Product');
+
+//Crear producto(solo para admin)
+const createProduct = async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json({ message: 'Error al crear producto'});
+  }
+};
+
+//Obtener todos los productos 
+const geAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: 'Error al obtener producto' });
+  }
+};
+
+//Actualizar producto
+const updateProduct = async (req, res) => {
+  try {
+    const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ message: 'Error al actualizar producto' })
+  }
+};
+
+//Eliminar producto
+const deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+  } catch (err) {
+    res.status(500).json({ message: 'Error al eliminar producto' })
+  }
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct
+};
