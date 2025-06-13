e11e683d55149bd0b01eb7eb1d9d76abdfeb853c
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-13 20:59:39 -0300

    creat routes for cart

---
+++ backend/routes/cartRoutes.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const router = express.Router();
+const {getCart, addToCart, updateCartItem, removeFromCart, clearCart} = require('../controllers/cartController');
+
+const auth = require('../middleware/auth'); //Protege rutas para usuarios logueados
+
+// Obtener el carrito del usuario logueado 
+router.get('/', auth, getCart);
+
+//Agregar un producto al carrito
+router.post('/add', auth, addToCart);
+
+// Actualizar cantidad de un producto en el carrito 
+router.put('/update', auth, updateCartItem);
+
+// Eliminar un producto del carrito
+router.delete('/remove', auth, removeFromCart);
+
+// Vaciar el carrito
+router.delete('/remove', auth, clearCart);
+
+module.exports = router;
