276a0719b2cbf2b406fdf8299705486b5a0ecb0b
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-13 17:15:32 -0300

    added isAdmin in Users

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   password: {type: String, required: true },
   isAdmin: {type: Boolean, default: false },
   verified: {type: Boolean, default: false },
+  isAdmin: {type: Boolean, default: false}
 }, {timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
