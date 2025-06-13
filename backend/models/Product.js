0bd8bf065224bc438f4e8852998565e2f960eb85
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-13 16:31:20 -0300

    create product.js

---
+++ backend/models/Product.js
@@ -0,0 +1,33 @@
+
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: true
+  },
+  stock: {
+    type: Number,
+    default: 0
+  },
+  category: {
+    type: String,
+    default: 'general'
+  },
+  imageUrl: String,
+  offer: {
+    type: Boolean,
+    default: false
+  },
+  offerPrice: Number
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Product', productSchema);
