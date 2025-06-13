a2e1071943d408809087d62ec83a5a2f6c7890b1
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-13 19:35:50 -0300

    create carts file

---
+++ backend/models/Cart.js
@@ -0,0 +1,32 @@
+//Este permite guardar productos que un usuario quiera comprar, antes de confirmar su pedido
+
+const mongoose = require('mongoose');
+
+const cartItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    requires: true,
+  },
+  quantity:{
+    type: Number,
+    required: true,
+    min: 1
+  }
+});
+
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true //Para que sea un carrito por usuario
+  },
+  items: [cartItemSchema],
+  updateAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.export = mongoose.model('Cart', cartSchema);
