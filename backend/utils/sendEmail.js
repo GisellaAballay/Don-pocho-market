ce5f19055b7d26d21bfc81a634ccd372b5d76e7f
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-14 19:24:54 -0300

    fixed file sendEmail try and catch

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,21 +1,30 @@
 
+const { text } = require('express');
 const nodemailer = require('nodemailer');
 
-const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+const sendEmail = async ({ to, subject, html }) => {
+  try{
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+    
+    const mailOptions = {
+      from: `"Don Pocho Market" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      text
+    };
 
-  await transporter.sendMail({
-    from: `"Don Pocho Market" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html
-  });
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email enviado: ', info.response);
+  } catch (error) {
+      console.error('Error enviando email:', error);
+      throw new Error('Error al enviar email');
+    };
 }
 
 module.exports = sendEmail;
