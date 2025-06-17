21231b757fecb0fb22102cf7733a4ebed96f5c56
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-17 16:00:07 -0300

    fixed error in function getUserOrders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
-const { createOrder, getUserOrder, getOrderById } = require('../controllers/orderController');
+const { createOrder, getUserOrders, getOrderById } = require('../controllers/orderController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/', authMiddleware, createOrder);    //Crear una nueva orden
 router.get('/', authMiddleware, getUserOrders);   //Ver todas las órdenes del usuario
 router.get('/:id', authMiddleware, getOrderById);   //Ver orden específica por ID 
-
 
 module.exports = router;
