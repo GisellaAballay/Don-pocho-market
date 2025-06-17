95737a9ae7e2bfdef74d74ee2817c39ecb03b0b5
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-17 14:13:38 -0300

    create routes for orders

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const router = express.Router();
+const { createOrder, getUserOrder, getOrderById } = require('../controllers/orderController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/', authMiddleware, createOrder);    //Crear una nueva orden
+router.get('/', authMiddleware, getUserOrders);   //Ver todas las órdenes del usuario
+router.get('/:id', authMiddleware, getOrderById);   //Ver orden específica por ID 
+
+
+module.exports = router;
