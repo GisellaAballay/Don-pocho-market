1299f4f1bae4c7981c1c5ece018b5360371df75e
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-18 12:10:19 -0300

    created notification preference to user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: {type: String, required: true },
-  email: {type:String, required: true, unique: true },
+  email: {type: String, required: true, unique: true },
   password: {type: String, required: true },
   isAdmin: {type: Boolean, default: false },
   verified: {type: Boolean, default: false },
-  isAdmin: {type: Boolean, default: false}
+  isAdmin: {type: Boolean, default: false},
+  notificationPreference: {type: String, enum: ['email', 'whatsapp'], default: 'email'},
+  phoneNumber: {type: String}
 }, {timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
