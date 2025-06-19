bdc3d68b78cf265673e7e675458e25ab899a80fb
GisellaAballay <gisellaaballaybonilla@gmail.com>
2025-06-19 16:51:17 -0300

    fixed syntax error in sendEmail file

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import { register, login, verifyEmail } from '../controllers/authController';
+import { register, login, verifyEmail } from '../controllers/authController.js';
 
 const router = express.Router();
 
